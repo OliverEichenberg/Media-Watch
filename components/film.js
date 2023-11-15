1a879abecd59050e2cc2b87bf37d27447e9fcc8c
OliverEichenberg <eichenbergoliver@gmail.com>
2023-11-14 23:30:13 -0500

    Finished outline design of film component

--- components/film.js
+++ components/film.js
@@ -1,17 +1,21 @@
 //React
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground } from 'react-native';
+
+import image from '../public/images/TestBanner.jpg'
 
 const Film = () => {
     return(
         <View style={styles.wrapper}>
-            <View style={{ width: '100%', height: '100%', position: 'relative' }}>
-                <View style={styles.footer}>
-                    <View style={{ backgroundColor: 'white', width: 50, borderRadius: 25}}><Text style={{ color: 'orange' }}>Testing</Text></View>
+            <ImageBackground source={image} style={styles.image}>
+                <View style={{ width: '100%', height: '100%', position: 'relative' }}>
+                        <View style={styles.footer}>
+                            <View style={styles.tag}><Text style={{ color: '#e7f2f9' }}>Testing</Text></View>
+                        </View>
                 </View>
-            </View>
+            </ImageBackground>
         </View>
-    )
-}
+        )
+    }
 
 const styles = StyleSheet.create({
     wrapper: {
@@ -21,7 +25,7 @@ const styles = StyleSheet.create({
         height: 140,
         width: 225,
         borderRadius: 25,
-        backgroundColor: 'orange'
+        backgroundColor: 'black',
     },
     footer: {
         flex: 1,
@@ -31,7 +35,17 @@ const styles = StyleSheet.create({
         paddingHorizontal: 15,
         height: '20%',
         width: '100%',
-        backgroundColor: 'green'
+        backgroundColor: '#1e2731'
+    },
+    tag: {
+        flex: 0,
+        alignItems: 'center',
+        backgroundColor: '#3a8ebc',
+        width: 60,
+        borderRadius: 25,
+    },
+    image: {
+        opacity: 0.5,
     }
 });
 
